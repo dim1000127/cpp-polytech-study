c646c047fa938a172b010ae00a377bf384da4c80
Дмитрий Лебедев <lebed@omegafuture.ru>
2024-02-24 23:33:28 +0300

    stl_list_deque: done 2b

--- OOP/STL_3_List_Deque/main.cpp
+++ OOP/STL_3_List_Deque/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <list>
+#include <ctime>
 #include "queue.h"
 
 
 int main() {
+    const int LIST_COUNT = 15;
+
     auto *queue = new Queue();
 
     queue->PutElementToQueue({.name = "Dima"},LOW);
@@ -18,6 +22,37 @@ int main() {
     queue->PutElementToQueue({.name = "Dima"}, HIGH);
 
     std::cout << queue->GetElementFromQueue().name << std::endl;
+
+    std::list<int> values;
+
+    std::srand(std::time(nullptr));
+
+    for (int i = 0; i < LIST_COUNT; i++) {
+        values.push_back(std::rand() % 20 + 1);
+    }
+
+    std::cout << "Source list" << std::endl;
+
+    for (auto value : values){
+        std::cout << value << " ";
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "List is certain order" << std::endl;
+    auto begin = values.begin();
+    auto end = values.end();
+
+    while (begin != end){
+        std::cout << *begin << " ";
+        end--;
+        if (begin != end){
+            std::cout << *end << " ";
+        }else{
+            break;
+        }
+        begin++;
+    }
 
     return 0;
 }
